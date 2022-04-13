2671ef2d924302e393ed674fe4ab8389d2cb51d4
FightM9 <fightm9@gmail.com>
2022-04-13 16:50:33 +0300

    chore: fix formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Routes } from "react-router-dom";
-import Header from "./components/Layouts/Header";
-import Country from "./pages/Country";
-import CountryDetails from "./pages/CountryDetails";
-import Error from "./pages/Error";
-import { fetchCounries } from "./store/reducer/CountriesSlice";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Layouts/Header';
+import Country from './pages/Country';
+import CountryDetails from './pages/CountryDetails';
+import Error from './pages/Error';
+import { fetchCounries } from './store/reducer/CountrySlice';
 
 export default function App() {
   const dispatch = useDispatch();
@@ -18,9 +18,9 @@ export default function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Country />} />
-        <Route path="country/:name" element={<CountryDetails/>} />
-        <Route path="*" element={<Error message="Page Not Found" />} />
+        <Route path='/' element={<Country />} />
+        <Route path='country/:name' element={<CountryDetails/>} />
+        <Route path='*' element={<Error message='Page Not Found' />} />
       </Routes>
     </>
   );
